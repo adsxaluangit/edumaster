5011726f7ff24d9929fdc393233b770cab632a92
adsxaluangit <adsxaluan@gmail.com>
2026-04-13 20:06:01 +0700

    fix(frontend): only fetch approved students when selecting class in decisions view

--- edumaster-frontend/views/DecisionsView.tsx
+++ edumaster-frontend/views/DecisionsView.tsx
@@ -431,7 +431,7 @@ const DecisionsView: React.FC<DecisionsViewProps> = ({ mode, currentUser }) => {
   // This scales to 500k+ students because we only fetch students for one class at a time
   const loadStudentsByClass = async (classDocId: string): Promise<Student[]> => {
     try {
-      const params = `filters[school_class][documentId][$eq]=${classDocId}&fields[0]=student_code&fields[1]=full_name&fields[2]=first_name&fields[3]=last_name&fields[4]=dob&fields[5]=pob&fields[6]=gender&fields[7]=id_number&fields[8]=is_approved&fields[9]=company&fields[10]=phone&populate[school_class]=true&pagination[pageSize]=500`;
+      const params = `filters[school_class][documentId][$eq]=${classDocId}&filters[is_approved][$eq]=true&fields[0]=student_code&fields[1]=full_name&fields[2]=first_name&fields[3]=last_name&fields[4]=dob&fields[5]=pob&fields[6]=gender&fields[7]=id_number&fields[8]=is_approved&fields[9]=company&fields[10]=phone&populate[school_class]=true&pagination[pageSize]=500`;
       const data = await fetchCategory(`${COLLECTIONS.STUDENTS}?${params}`);
       if (!data) return [];
       return data.map((d: any) => {
@@ -454,7 +454,7 @@ const DecisionsView: React.FC<DecisionsViewProps> = ({ mode, currentUser }) => {
           className: classData?.attributes?.name || classData?.name || '',
           classCode: classData?.attributes?.code || classData?.code || '',
           classId: String(classData?.documentId || classData?.id || ''),
-          isApproved: !!d.is_approved,
+          isApproved: d.is_approved === true || String(d.is_approved).toLowerCase() === 'true',
           documents: [],
           photo: null
         } as Student;
